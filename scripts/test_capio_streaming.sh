5e28f3a52d7cda50e097dcf8824441598bc29cad
FalkenRambaldi <alberto.riccardo.martinelli@gmail.com>
2020-12-02 17:33:36 +0100

    minor fix scripts

--- scripts/test_capio_streaming.sh
+++ scripts/test_capio_streaming.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mpiexec -n 2 ~/capio/build/capio_process/capio ../one_node_4_4.yaml > streaming_capio_process_2_2.txt &
+mpiexec -n 2 ~/capio/build/capio_process/capio 10010 ~/capio/one_node_4_4.yaml > streaming_capio_process_2_2.txt &
 sleep .1
 mpiexec -n 4 ../build/benchmark/capio_read_write/par_read_capio ~/capio/one_node_4_4.yaml ../configurations_examples/balanced_config.txt abbr streaming > streaming_4_4_cons.txt &
 sleep .1
